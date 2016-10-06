e8afcde935eab6ecbdb8446038c2e7670537e6ee
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-06 12:31:45 +0100

    Game running, but bug with holding / incrementing player scores

--- day2/task16.groovy
+++ day2/task16.groovy
@@ -1,5 +1,3 @@
-import java.io
-
 Main.main()
 
 class Main{
@@ -25,30 +23,54 @@ class Game{
     int numPlayers = 2
     int currentPlayer = 1
     int turn = 1
-    int[] scores = new int[]{0, 0}
-    String[] currentTurns = new String[]{"", ""}
-    String[] validTurns = new String[]{"R", "P", "S"}
+    int[] scores = new int[2]
+    String[] currentTurns = new String[2]
+    String[] validTurns
     boolean gameRunning = true
     String player1 = "Player 1"
     String player2 = "Player 2"
 
-    public game(String player1 = null, String player2 = null){
+    public Game(String player1 = null, String player2 = null){
 
-        if(player1 != null) this.player1 = player1
-        if(player2 != null) this.player2 = player2
+        if(player1 != null && player1 != "")
+            this.player1 = player1
 
-        while(gameRunning)
+        if(player2 != null && player2 != "")
+            this.player2 = player2
+
+        init()
+
+        while(gameRunning){
             takeTurn()
+        }
     }
 
-    private void clearTurns(){
-        currentTurns = {"", ""}
+    private void init(){
+        clearScores()
+        clearCurrentTurns()
+        setValidTurn()
+    }
+
+    private void clearScores(){
+        this.scores = new int[2]
+    }
+
+    private void setValidTurn(){
+        this.validTurns = new String[3]
+        this.validTurns[0] = "R"
+        this.validTurns[1] = "P"
+        this.validTurns[2] = "S"
+    }
+
+    private void clearCurrentTurns(){
+        this.currentTurns = new String[2]
     }
 
     private void takeTurn(){
-        while(this.currentPlayer <= this.numPlayers)
-            println String.format("Player %1s, please enter your turn ([R]ock, [P]aper, [S]cissors):", this.currentPlayer)
-            this.currentTurn[currentPlayer-1] = java.io.Console.readPassword()
+        while(this.currentPlayer <= this.numPlayers){
+            String playerName = this.currentPlayer == 1 ? this.player1 : this.player2
+            println String.format("%1s, please enter your turn ([R]ock, [P]aper, [S]cissors):", playerName)
+            this.currentTurns[currentPlayer-1] = System.console().readLine()
             if(checkTurn(this.currentPlayer)){
                 this.currentPlayer++
             }
@@ -81,7 +103,7 @@ class Game{
                 break
 
             case "P":
-                scorePaperkPlay(player2TurnCode)
+                scorePaperPlay(player2TurnCode)
                 break
 
             case "S":
@@ -95,14 +117,17 @@ class Game{
             case "R":
                 incrementPlayer1Score()
                 incrementPlayer2Score()
+                roundDrawMessage()
                 break
 
             case "P":
                 incrementPlayer2Score()
+                roundWinMessage(this.player2)
                 break
 
             case "S":
                 incrementPlayer1Score()
+                roundWinMessage(this.player1)
                 break
         }
     }
@@ -111,15 +136,18 @@ class Game{
         switch(against){
             case "R":
                 incrementPlayer1Score()
+                roundWinMessage(this.player1)
                 break
 
             case "P":
                 incrementPlayer1Score()
                 incrementPlayer2Score()
+                roundDrawMessage()
                 break
 
             case "S":
                 incrementPlayer2Score()
+                roundWinMessage(this.player2)
                 break
         }
     }
@@ -128,25 +156,46 @@ class Game{
         switch(against){
             case "R":
                 incrementPlayer2Score()
+                roundWinMessage(this.player2)
                 break
 
             case "P":
                 incrementPlayer1Score()
+                roundWinMessage(this.player1)
                 break
 
             case "S":
                 incrementPlayer1Score()
                 incrementPlayer2Score()
+                roundDrawMessage()
                 break
         }
     }
 
+    private void roundDrawMessage(){
+        println "Draw " + getScoreText()
+    }
+
+    private void roundWinMessage(String winner){
+        println winner + " has won this round "  + getScoreText()
+    }
+
+    private String getScoreText(){
+        return "| " + this.player1 + ": " + getPlayer1Score() + " | " + this.player2 + ": " + getPlayer2Score() + " |"
+    }
+
     private void incrementPlayer1Score(){
+        if(this.scores[0] == null)
+            this.scores[0] = 0
+
         this.scores[0]++
         checkWinCondition()
     }
 
     private void incrementPlayer2Score(){
+        if(this.scores[1] == null)
+            this.scores[1] = 0
+
         this.scores[1]++
         checkWinCondition()
     }
@@ -162,15 +211,15 @@ class Game{
     private void checkWinCondition(){
         int scoreDiff = getPlayer1Score() - getPlayer2Score()
 
-        String winner = scoreDiff > 0 ? "1" : "2"
+        String winner = scoreDiff > 0 ? this.player1 : this.player2
 
         if (Math.abs(scoreDiff) > 3){
             this.gameRunning = false
-            println "Congratulations Player" + winner + ". You have won this round."
+            println "Congratulations Player" + winner + ". You have won this game."
         }
         else{
             this.currentPlayer = 1;
-            this.currentTurn = new int[]{0,0}
+            clearScores()
         }
     }
 
