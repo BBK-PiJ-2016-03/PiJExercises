1497bc9f472c300c81868f43dd2059641fe6d798
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-06 11:35:50 +0100

    Added task 16 scoring and win condition checks

--- day2/task16.groovy
+++ day2/task16.groovy
@@ -25,10 +25,11 @@ class Main{
                 currentPlayer++
             }
         }
+        scoreRound()
     }
 
     private boolean checkTurn(int player){
-        if(validTurns.contains(getTurnCode(getPlayerTurn(player))))
+        if(validTurns.contains(getPlayerTurn(player)))
             return true
 
         return false
@@ -39,7 +40,104 @@ class Main{
     }
 
     private String getPlayerTurn(int player){
-        return currentTurns[player-1]
+        return getTurnCode(currentTurns[player-1])
+    }
+
+    private void scoreRound(){
+        String player1TurnCode = getPlayerTurn(1)
+        String player2TurnCode = getPlayerTurn(2)
+
+        switch(player1TurnCode){
+            case "R":
+                scoreRockPlay(player2TurnCode)
+                break
+
+            case "P":
+                break
+
+            case "S":
+                break
+        }
+    }
+
+    private void scoreRockPlay(String against){
+        switch(against){
+            case "R":
+                incrementPlayer1Score()
+                incrementPlayer2Score()
+                break
+
+            case "P":
+                incrementPlayer2Score()
+                break
+
+            case "S":
+                incrementPlayer1Score()
+                break
+        }
+    }
+
+    private void scorePaperPlay(String against){
+        switch(against){
+            case "R":
+                incrementPlayer1Score()
+                break
+
+            case "P":
+                incrementPlayer1Score()
+                incrementPlayer2Score()
+                break
+
+            case "S":
+                incrementPlayer2Score()
+                break
+        }
+    }
+
+    private void scoreScissorsPlay(String against){
+        switch(against){
+            case "R":
+                incrementPlayer2Score()
+                break
+
+            case "P":
+                incrementPlayer1Score()
+                break
+
+            case "S":
+                incrementPlayer1Score()
+                incrementPlayer2Score()
+                break
+        }
+    }
+
+    private void incrementPlayer1Score(){
+        scores[0]++
+        checkWinCondition()
+    }
+
+    private void incrementPlayer2Score(){
+        scores[1]++
+        checkWinCondition()
+    }
+
+    private int getPlayer1Score(){
+        return scores[0]
+    }
+
+    private int getPlayer2Score(){
+        return scores[1]
+    }
+
+    private void checkWinCondition(){
+        int scoreDiff = getPlayer1Score - getPlayer2Score
+
+        String winner = scoreDiff > 0 ? "1" : "2"
+
+        if (Math.abs(scoreDiff) > 3){
+            gameRunning = false
+            println "Congratulations Player" + winner + ". You have won this round."
+        }
     }
 
 }
