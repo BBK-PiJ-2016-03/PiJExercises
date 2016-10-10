62dbe1b73367ad1d075c405d8d61e48e999a418c
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-10 14:42:23 +0100

    Implemented, started to implement receiving args from command line

--- day3/task3.groovy
+++ day3/task3.groovy
@@ -7,3 +7,84 @@ operands, and then execute it.  For example, if the user enters “3
 enters “23 * 4” the program outputs “92”.
 */
 
+validSum = false
+String[] operators = Arrays.asList("\\+", "-", "\\*", "/")
+double inputOperand1
+double inputOperand2
+
+String userInput = args.join("")
+
+userInput = userInput.replace(" ", "")
+
+while(!validSum){
+
+    if(userInput == null || userInput == ""){
+        print "Please enter your two operand, single operator sum: "
+        userInput = System.console().readLine()
+    }
+
+    String[] sum = new String[1]
+    String operator = ""
+
+    int i = 0
+    while(sum.size() != 2 && i < operators.size()){
+        operator = operators[i]
+        sum = userInput.split(operator)
+        i++
+    }
+
+    if(sum.size() != 2){
+        println "Please enter a valid sum"
+    }
+    else{
+        try{
+            inputOperand1 = Double.parseDouble(sum[0].trim())
+            inputOperand2 = Double.parseDouble(sum[1].trim())
+
+            Double[] numbers = new Double[2]
+            numbers[0] = inputOperand1
+            numbers[1] = inputOperand2
+
+            println calculate(numbers, operator.replace("\\", ""))
+            validSum = true
+        }
+        catch(NumberFormatException e){
+            println "Your operands were not valid"
+        }
+    }
+
+    userInput == null
+
+}
+
+def calculate(numbers, operator){
+
+    double precision = 1E14
+
+    if(numbers.size() < 1)
+        return null
+
+    double result = numbers[0]
+
+    for(int i = 1; i < numbers.size(); i++) {
+        switch(operator){
+            case "+":
+                result += numbers[i]
+                break
+
+            case "-":
+                result -= numbers[i]
+                break
+
+            case "/":
+                result /= numbers[i]
+                break
+
+            case "*":
+                result *= numbers[i]
+                break
+        }
+    }
+
+    return Math.round(result * precision) / precision
+}
