613189b22d7e0360ba27073291f8e143a8ca6ce6
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-10 16:18:49 +0100

    Started work on task5

---
+++ week2/day3/task5.groovy
@@ -0,0 +1,23 @@
+/*5  Rectangle
+Write a program that reads the X and Y coordinates of two points an
+d then outputs the area of a rectangle where
+both points are opposite corners. Use the following class to store t
+he data for the points:
+class Rectangle {
+Point upLeft;
+Point downRight;
+}
+Your program should calculate (and write on the screen) the perime
+ter and area of the rectangle.
+Note: For exercises 5, 6, and 7 you must access (i.e. read or write)
+the value of the coordinates of the points
+through the rectangle, not directly through the point, i.e.
+myRectangle.upLeft.x , not point.x or x. */
+
+
+
+
+class Rectangle {
+    Point upLeft;
+    Point downRight;
+}
