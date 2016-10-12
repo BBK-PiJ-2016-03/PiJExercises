df6ea39aaf5cff3e4af3d78c6782b10620b1066a
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-12 11:50:16 +0100

    Task 10 initial commit

---
+++ week2/day3/task10.groovy
@@ -0,0 +1,4 @@
+/*10  Counting pairs of letters (*)
+Write a program that reads a short string and then some longer text. The program must say how many times you
+can find the short string in the text. You cannot use any method of String apart from charAt() and length(). */
+
