0b21a0ddce62c572885ce111002ee60a6a99a8f0
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-12 12:33:19 +0100

    Fixed error using primitive as list generic

--- week2/day3/task11.groovy
+++ week2/day3/task11.groovy
@@ -1,26 +1,28 @@
 print "Please enter some text: "
 String userText = System.console().readLine()
 
-List<char> characters = new ArrayList<>()
+List<String> characters = new ArrayList<>()
 String userCharacter = ""
 
 while(!characters.contains(userCharacter)){
     print "Please enter the character you would like to count (e): "
     userCharacter = System.console().readLine()
 
+    characters.add(userCharacter)
+
     if(userCharacter == "" || userCharacter == null)
     userCharacter = "e"
 
-    char target = userCharacter[0]
+    String target = userCharacter[0]
 
     println "In [" + userText + "]"
     println "There are " + countCharacterOccurence(target, userText) + " instances of " + target
 }
 
 
-def countCharacterOccurence(char targetCharacter, String text){
+def countCharacterOccurence(String targetCharacter, String text){
     int count = 0;
-    for(char character : text){
+    for(String character : text){
         if(targetCharacter == character)
             count++
     }
