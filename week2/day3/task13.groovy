8683d1f3d1906e421b329c7876b78e5325862566
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-13 18:15:47 +0100

    Task fixed method braces, output display, handled negative response

--- week2/day3/task13.groovy
+++ week2/day3/task13.groovy
@@ -5,13 +5,16 @@ print("Please enter your text: ");
 String words = System.console().readLine();
 String wordsReversed = reverse(words);
 
-if(words == wordsReversed){
-    println("[" + words + "]is a palindrome");
-}
+
+String response = (words == wordsReversed) ? "is" : "is not";
+
+println("[" + words + "] " + response + " a palindrome");
+
+
 
 def reverse(String words){
     String reversed = "";
-    for(int i = words.length - 1; i >= 0; i--){
+    for(int i = words.length() - 1; i >= 0; i--){
         reversed += words[i]
     }
     return reversed
