08c793be2f56eded7e35daa0800a1fbbda6249aa
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-13 18:19:01 +0100

    COmpleted task 14 using task13 as a basis

---
+++ week2/day3/task14.groovy
@@ -0,0 +1,16 @@
+// 14 Palindrome creator
+
+println("Palindrome creator");
+print("Please enter your text: ");
+String words = System.console().readLine();
+String wordsReversed = reverse(words);
+
+println("Your palindrom is: " + words+wordsReversed)
+
+def reverse(String words){
+    String reversed = "";
+    for(int i = words.length() - 1; i >= 0; i--){
+        reversed += words[i]
+    }
+    return reversed
+}
