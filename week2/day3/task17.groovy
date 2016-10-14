bf4696821d73c5af8f701d4354b69ae2fc7069f7
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-14 14:14:23 +0100

    Task 17 - working commands, state machine untested

--- week2/day3/task17.groovy
+++ week2/day3/task17.groovy
@@ -1,72 +1,130 @@
 //17 Mail server (*)
-enum State{
-     MAILFROM,
-     RCPTFROM,
-     DATA
-}
 
-State currentState = State.MAILFROM;
+MailServer server = new MailServer();
+server.Start();
 
-println("Welcome to My Mail Server!");
-
-booean running = true;
-
-while(running){
-
-    getCommand();
-
-}
-
-
-def checkCommand(String check, String command){
-
-    if(command.contains(check)){
-        String email = command.replace(check, "");
-        return isValidEmail(email);
+class MailServer{
+    enum State{
+        READY
+        MAILFROM,
+        RCPTTO,
+        DATA
     }
 
-    return false;
-}
+    private State currentState = State.MAILFROM;
+    private String fromAddress = "";
+    private String toAddress = "";
+    private String body = "";
 
-def isValidEmail(String email){
-    email = email.trim();
-    int countAt = 0;
 
-    for(int i = 0; i < email.length(); i++){
-        if(c == '@'){
-            countAt++;
-            if(i == 0 || countAt > 1 || i == (email.length() - 1)){
-                println("Invalid email");
-                return false;
-            }
+    public Start(){
+        println("Welcome to My Mail Server!");
+
+        boolean running = true;
+
+        while(running){
+
+            getCommand();
+
         }
     }
-    return true;
-}
 
-def getCommand(){
-    boolean validCommand = false;
+    private boolean checkCommand(String check, String command){
 
-    while(!validCommand){
-        print(">>>");
-        String command = System.console().readLine();
-        validCommand = verifyCommand(command);
-        if(!validCommand)
-            println("Invalid command.");
+        if(command.contains(check)){
+            String email = command.replace(check, "");
+            return isValidEmail(email);
+        }
+
+        return false;
     }
 
-    println("OK");
+    private boolean isValidEmail(String email){
+        email = email.trim();
+        int countAt = 0;
+
+        for(int i = 0; i < email.length(); i++){
+            if(email[i] == '@'){
+                countAt++;
+                if(i == 0 || countAt > 1 || i == (email.length() - 1)){
+                    println("Invalid email");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void getCommand(){
+        boolean validCommand = false;
+
+        while(!validCommand){
+            print(">>> ");
+            String command = System.console().readLine();
+            validCommand = verifyCommand(command);
+            if(!validCommand)
+                println("Invalid command.");
+        }
+
+        println("OK");
+    }
+
+    private boolean verifyCommand(String command){
+        switch(this.currentState){
+            case State.READY:
+                boolean accepted = checkCommand("MAIL FROM: ", command);
+                if(accepted){
+                    this.fromAddress = command.replace("MAIL FROM: ", "").trim();
+                    this.currentState = State.MAILFROM;
+                }
+                return accepted;
+            case State.MAILFROM:
+                boolean accepted = checkCommand("RCPT TO: ", command);
+                if(accepted){
+                    this.toAddress = command.replace("MAIL FROM: ", "").trim();
+                    this.currentState = State.RCPTTO;
+                }
+                return accepted;
+            case State.RCPTTO:
+                boolean accepted = checkCommand("DATA: ", command);
+                if(accepted){
+                    this.currentState = State.DATA;
+                }
+                return accepted;
+            case State.DATA:
+                if(command == "."){
+                    sendEmail();
+                    this.currentState = State.READY;
+                }
+                else{
+                    this.data += command.trim();
+                }
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private void sendEmail(){
+        println("Sending email...");
+        println("from: " + fromAddress);
+        println("to: " + toAddress);
+        println(body);
+    }
+
+    /*
+    println("Commands:");
+    println("MAIL FROM: email@address, to set sender email address.");
+    println("RCPT TO: email@address, to set fdetination email address.");
+    println("DATA to set the body of the address.");
+    println("Enter a line containing only a . to finish entering data.");
+    println("QUIT at any other point enter to exit the program");
+    println("");
+    */
+
 }
 
-def verifyCommand(command){
-    switch(currentState){
-        case State.MAILFROM:
-            return checkCommand("", command);
-        case State.RCPTFROM:
-            return checkCommand("", command);
-        case State.DATA:
-            return true;
-        default:
-            return false;
-    }
-}
+
+
+
+
