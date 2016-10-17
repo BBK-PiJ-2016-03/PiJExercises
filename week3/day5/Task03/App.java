283b27ca91b761dc7f9c5163deba0d40ca893dbf
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-17 15:45:09 +0100

    Added last manipulation type

--- week3/day5/Task03/App.java
+++ week3/day5/Task03/App.java
@@ -7,9 +7,13 @@ public class App{
 
         matrix.setRow(1, "1,2,3,4,5");
 
+        matrix.setElement(2,4,9);
+
         System.out.println(matrix.toString());
 
         System.out.println(matrix.prettyPrint());
+
+
 
     }
 }
