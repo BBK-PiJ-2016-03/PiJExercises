a8bec3b6de80929a1e82ded5cf3adbcd8577c067
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-17 12:17:08 +0100

    Fixed getters missing brackets and erroneous comma

--- week2/day4/task05.groovy
+++ week2/day4/task05.groovy
@@ -4,16 +4,16 @@ class Point{
 
     private double x, y;
 
-    public Point(double x, ,double y){
+    public Point(double x, double y){
         this.x = x;
         this.y = y;
     }
 
-    public double getX{
+    public double getX(){
         return this.x;
     }
 
-    public double getY{
+    public double getY(){
         return this.y;
     }
 
