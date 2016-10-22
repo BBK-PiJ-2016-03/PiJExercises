f16e4214fdcac11bb165017e124ba0857263d03c
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-22 14:29:39 +0100

    Hospital linked list initial commit (late)

---
+++ week3/day6/task01/HospitalManager.java
@@ -0,0 +1,62 @@
+public class HospitalManager{
+    private Patient firstPatient;
+
+    //O(1)
+    public HospitalManager(){
+        this.firstPatient = null;
+    }
+
+    //O(N)
+    public void addPatient(Patient newPatient){
+        Patient currentPatient = this.firstPatient;
+
+        if(currentPatient == null){
+            this.firstPatient = newPatient;
+            return;
+        }
+
+        while(currentPatient.getNextPatient() != null){
+            currentPatient = currentPatient.getNextPatient();
+        }
+
+        currentPatient.setNextPatient(newPatient);
+    }
+
+    //O(N)
+    public void removePatient(String name){
+        Patient currentPatient = this.firstPatient;
+
+        if(currentPatient.getName().equals(name)){
+            this.firstPatient = currentPatient.getNextPatient();
+        }
+
+        while(currentPatient.getNextPatient() != null){
+            if(tryToRemoveNextPatient(name, currentPatient))
+                break;
+            currentPatient = currentPatient.getNextPatient();
+        }
+    }
+
+    private boolean tryToRemoveNextPatient(String name, Patient previous){
+        Patient candidateToRemove = previous.getNextPatient();
+
+        if(candidateToRemove == null)
+            return false;
+
+        if(candidateToRemove.getName().equals(name)){
+                previous.setNextPatient(candidateToRemove.getNextPatient());
+                return true;
+        }
+        return false;
+    }
+
+    //O(N)
+    public void printPatients(){
+        Patient currentPatient = this.firstPatient;
+
+        while(currentPatient != null){
+            System.out.println(currentPatient);
+            currentPatient = currentPatient.getNextPatient();
+        }
+    }
+}
