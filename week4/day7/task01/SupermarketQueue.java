fb0d1a1290605c7063bfc118dca7891984168425
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-24 14:42:56 +0100

    Implemented SupermarketQueue

--- week4/day7/task01/SupermarketQueue.java
+++ week4/day7/task01/SupermarketQueue.java
@@ -1,10 +1,54 @@
 public class SupermarketQueue implements PersonQueue {
-    public void insert(Person person) {
 
+    private Person firstPerson = null;
+    private Person lastPerson = null;
+
+    public SupermarketQueue(){
+
+    }
+
+    public SupermarketQueue(Person person){
+        insert(person);
+    }
+
+    public void insert(Person person) {
+        if(firstPerson == null){
+            firstPerson = person;
+            lastPerson = person;
+            return;
+        }
+
+        person.setNextPerson(firstPerson);
+        firstPerson.setPrevPerson(person);
+        firstPerson = person;
     }
 
     public Person retrieve() {
 
+        Person removedPerson = lastPerson;
+
+        if(lastPerson == null)
+            return null;
+
+        if(lastPerson == firstPerson){
+            lastPerson.setNextPerson = null;
+            lastPerson.setPrevPerson = null;
+            firstPerson = null;
+            lastPerson = null;
+            return removedPerson;
+        }
+
+        lastPerson = lastPerson.getPrevPerson();
+        lastPerson.setNextPerson(null);
+
+        removedPerson.setPrevPerson(null);
+        removedPerson.setNextPerson(null);
+
+        return removedPerson;
+    }
+
+    public Person peek(){
+        return lastPerson;
     }
 
 }
