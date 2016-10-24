5cc6a40aff4506f70a9ceeea407cae4f749cbd86
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-24 15:42:00 +0100

    Fixed println error

--- week4/day7/task05.1/App.java
+++ week4/day7/task05.1/App.java
@@ -3,7 +3,7 @@ public class App{
 
         String input = "";
         while(input != "q" && input != "quit"){
-            println "Give me a string and I will calculate its hash code (q to quit)";
+            System.out.println("Give me a string and I will calculate its hash code (q to quit)");
             String str = System.console().readLine();
             int hash = str.hashCode();
             int smallHash = HashUtilities.shortHash(hash);
