69c4c1639cdf251b79001ba248e8257ee95d3d6f
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-24 13:53:08 +0100

    initial commit

---
+++ week4/day7/task01/Supermarket.java
@@ -0,0 +1,10 @@
+public class Supermarket{
+
+    public void addPerson(){
+    }
+
+    public void servePerson(){
+
+    }
+
+}
---
+++ week4/day7/task01/SupermarketQueue.java
@@ -0,0 +1,3 @@
+public class SupermarketQueue implements PersonQueue {
+
+}
