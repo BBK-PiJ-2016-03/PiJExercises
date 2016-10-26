d2ec0e0da6813d13fb483a05db0b3f2b562ece18
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-26 13:28:24 +0100

    Initial Commit for task1

---
+++ week4/day8/task01/MobilePhone.java
@@ -0,0 +1,21 @@
+public class MobilePhone extends OldPhone{
+
+    private String[] lastNumbers;
+
+    public MobilePhone(){
+        lastNumbers = new String[10];
+    }
+
+    public void ringAlarm(String){
+
+    }
+
+    public void playGame(String){
+
+    }
+
+    public void printLastNumbers(){
+
+    }
+
+}
