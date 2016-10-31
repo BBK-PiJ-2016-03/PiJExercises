85b7a69539e075de019ef621ec5f42e74271f3ae
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-10-31 15:06:00 +0000

    Task 1 and 2.1

---
+++ week5/day9/task02/App.java
@@ -0,0 +1,41 @@
+import java.lang.reflect.*;
+
+public class App{
+    public static void main(String[] args){
+        SmartPhone myPhone = new SmartPhone();
+
+        myPhone.browseWeb("www.google.com");
+        System.out.println(myPhone.findPosition());
+        myPhone.playGame("Snake");
+        myPhone.call("01708731555");
+
+
+        /*
+        
+        Method[] methods = SmartPhone.class.getDeclaredMethods();
+
+        for(Method method : methods){
+
+            if(!method.isAccessible())
+                method.setAccessible(true);
+
+            System.out.println(method.getName());
+
+            Object[] params = method.getGenericParameterTypes();
+            
+            if(params.length == 0){
+                try{
+                    //System.out.println(method.invoke(myPhone));
+                    method.invoke(new SmartPhone());
+                }
+                catch(IllegalAccessException e){
+
+                }
+            }
+            
+        }
+
+        */
+    }
+
+}
