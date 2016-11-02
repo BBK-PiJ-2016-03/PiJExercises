64641e39f33914e9f4b1422fc43c61f20d01e783
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-02 15:07:20 +0000

    Tested for power of 3

--- week5/day10/task05/Power.java
+++ week5/day10/task05/Power.java
@@ -3,7 +3,7 @@ public class Power{
     public static void main(String[] args){
         Power power = new Power();
         for(int i = 0; i < 8; i++){
-            System.out.println(String.format("%d: %d", i, power.pow(i, 2)));
+            System.out.println(String.format("%d: %d", i, power.pow(i, 3)));
         }
         
     }
