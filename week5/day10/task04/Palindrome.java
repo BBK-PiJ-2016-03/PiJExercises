07a82315d5bf361568406c683f3ea09e0720aa0a
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-02 14:39:45 +0000

    fixed substring bug for recursive call

--- week5/day10/task04/Palindrome.java
+++ week5/day10/task04/Palindrome.java
@@ -1,17 +1,22 @@
 public class Palindrome{
     public static void main(String[] args){
         Palindrome p = new Palindrome();
-        System.out.println(p.check(p.getWord()));
+        while(true){
+            System.out.println(p.check(p.getWord()));
+        }
+        
     }
 
     private String getWord(){
-        return System.console.readLine();
+        System.out.print("Please enter a word to check if it is a palindrome: ");
+        return System.console().readLine();
     }
 
     private boolean check(String word){
         if(word.length() <= 1)
             return true;
         
-        return word[0] == word[word.length()-1] && check(word.substring(1,word.length()-2));
+        int lastCharIndex = word.length()-1;
+        return word.charAt(0) == word.charAt(lastCharIndex) && check(word.substring(1,lastCharIndex));
     }
 }
