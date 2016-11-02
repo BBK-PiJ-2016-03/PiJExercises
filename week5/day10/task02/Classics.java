5de12bc7fb16f28f3178e77451c482d19c092864
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-02 12:41:13 +0000

    Fixed formatting so both methods output the same

--- week5/day10/task02/Classics.java
+++ week5/day10/task02/Classics.java
@@ -2,28 +2,32 @@ public class Classics{
     public static void main(String[] args){
         Classics classics = new Classics();
 
-        System.out.print(String.format("4! = %d :: \n", classics.factorialIterative(4)));
+        System.out.print("4! : ");
+        System.out.print(String.format(" = %d\n", classics.factorialIterative(4)));
 
-        System.out.print(String.format("4! = %d :: \n", classics.factorialRecursive(4)));
+        System.out.print("4! : ");
+        System.out.print(String.format(" = %d\n", classics.factorialRecursive(4)));
 
     }
 
     private int  factorialIterative(int n) {
         int total = 1;
-        
-        System.out.print("1 * ");
-        for(int i = 2; i <= n; i++){
+        for(int i = n; i >= 1; i--){
             total *= i;
-            System.out.print(String.format(" * %d ", i));
+            String output = i > 1 ? String.format("%d * ", i) : String.format("%d", i);
+            System.out.print(output);
         }
         return total;
     }
 
     private int factorialRecursive(int n) {
-        if(n <= 0)
+        if(n <= 0){
             return 1;
+        }
 
-        System.out.print(String.format(" %d * ", n));
+        String output = n > 1 ? String.format("%d * ", n) : String.format("%d", n);        
+        System.out.print(output);
+
         return n * factorialRecursive(n-1);
     }
 }
