3fa2f2f45406779421910afb61ea730300b1b0ed
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-02 12:29:38 +0000

    first pass on classics

---
+++ week5/day10/task02/Classics.java
@@ -0,0 +1,28 @@
+public class Classics{
+    public static void main(String[] args){
+        Classics classics = new Classics();
+
+        System.out.print(String.format("4! = %d :: \n", classics.factorialIterative(4)));
+
+        System.out.print(String.format("4! = %d :: \n", classics.factorialRecursive(4)));
+
+    }
+
+    private int  factorialIterative(int n) {
+        int total = 1;
+        
+        System.out.print("1 * ");
+        for(int i = 2; i <= n; i++){
+            total *= i;
+            System.out.print(String format(" * %d ", i));
+        }
+    }
+
+    private int factorialRecursive(int n) {
+        if(n <= 0)
+            return 1;
+
+        System.out.print(String.format(" %d * ", n));
+        return n * factorialRecursive(n-1);
+    }
+}
