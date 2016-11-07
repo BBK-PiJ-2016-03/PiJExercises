f2ed23f767a62b077914f623a788bcd1e49e5012
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-07 14:02:23 +0000

    Implemented methods from the notes

--- week6/day11/IntegerTreeNode.java
+++ week6/day11/IntegerTreeNode.java
@@ -3,4 +3,53 @@ public class IntegerTreeNode{
     IntegerTreeNode left;
     IntegerTreeNode right;
 
+    public void add(int newNumber){
+        if (newNumber > this.value) {
+            if (right == null) {
+                right = new IntegerTreeNode(newNumber);
+            } 
+            else {
+                right.add(newNumber);
+            }
+        } 
+        else {
+            if (left == null) {
+                left = new IntegerTreeNode(newNumber);
+            } 
+            else {
+                left.add(newNumber);
+            }
+        }
+    }
+
+    public boolean contains(int checkValue){
+        if (n == this.value) {
+            return true;
+        } 
+        else if (n > this.value) {
+            if (right == null) {
+                return false;
+            } 
+            else {
+                return right.contains(n);
+            }
+        } 
+        else {
+            if (left == null) {
+                return false;
+            } 
+            else {
+                return left.contains(n);
+            }
+        }
+    }
+
+    public int getMax(){
+
+    }
+
+    public int getMin(){
+
+    }
+
 }
