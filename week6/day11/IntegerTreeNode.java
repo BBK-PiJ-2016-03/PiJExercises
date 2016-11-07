7bca6a26a72072f5274ff81c31d9ce27acda2368
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-07 14:30:33 +0000

    Added a constructor

--- week6/day11/IntegerTreeNode.java
+++ week6/day11/IntegerTreeNode.java
@@ -22,7 +22,7 @@ public class IntegerTreeNode{
         }
     }
 
-    public boolean contains(int checkValue){
+    public boolean contains(int n){
         if (n == this.value) {
             return true;
         } 
@@ -45,11 +45,17 @@ public class IntegerTreeNode{
     }
 
     public int getMax(){
+        if(this.right == null)
+            return this.value;
 
+        return this.right.getMax();
     }
 
     public int getMin(){
-
+        if(this.left == null)
+            return this.value;
+        
+        return this.left.getMin();
     }
 
 }
