3d34b7c2d0ad3c460cfc964a670c5678a165b37d
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-09 17:21:55 +0000

    implemented isSymmetrical

--- week3/day5/Task02/ArrayChecker.java
+++ week3/day5/Task02/ArrayChecker.java
@@ -1,47 +1,41 @@
 public class ArrayChecker{
+
     public static void main(String[] args){
         ArrayChecker ac = new ArrayChecker();
-        int[] arr = new int[]{1,2,3,4,5,4,3,2,1};
+        int[] arr = new int[]{1,2,2,1};
         System.out.println("Symmetrical (true): " + ac.isSymetrical(arr));
 
-        int[] arr2 = new int[]{1,1,2,3,4,5,4,3,2,1};
-        System.out.println("Symmetrical (false): " + ac.isSymetrical(arr2));
+        int[] arr2 = new int[]{1,2,3,2,1};
+        System.out.println("Symmetrical (true): " + ac.isSymetrical(arr2));
+
+        int[] arr3 = new int[]{1,1,2,3,4,5,4,3,2,1};
+        System.out.println("Symmetrical (false): " + ac.isSymetrical(arr3));
+
+        int[] arr4 = new int[]{1,1,2,3,4,5,4,3,2};
+        System.out.println("Symmetrical (false): " + ac.isSymetrical(arr4));
     }
 
     private boolean isSymetrical(int[] arr){   
+        boolean symmetric = true;
+        int midPoint = (arr.length - 1) / 2;
 
-        if(arr.length < 2)
-            return true;
-
-        if(arr.length == 2)
-            return arr[0] == arr[1];
-
-        if(arr[0] != arr[arr.length - 1]){
-            return false;
+        for(int i = 0; i <= midPoint; i++){
+            symmetric = checkSymmetric(arr, i);
+            if(!symmetric)
+                break;
         }
 
-        return isSymetrical(copy(arr, 1, arr.length - 2));
+        return symmetric;
+    }
+
+    private boolean checkSymmetric(int[] arr, int index){
+        if(arr[index] == arr[arr.length - 1 - index]){
+            return true;
+        }
+        return false;
     }
 
     private int[] reverse(int[] arr){
         return new int[0];
-    }
-
-    public int[] copy(int[] source, int startIndex, int endIndex){
-
-        int size = endIndex - startIndex + 1;
-
-        if(size < 1 || size > source.length)
-            return null;
-        
-        int[] destination = new int[size];
-
-        int j = 0;
-        for(int i = startIndex; i < destination.length; i++){
-            destination[j] = source[i];
-            j++;
-        }
-
-        return destination;
     }
 }
