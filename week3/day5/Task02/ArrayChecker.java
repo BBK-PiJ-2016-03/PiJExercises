6d4cc29c25327d19f1e60507b769f02979731657
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-09 16:55:51 +0000

    Implemented isSymmetrical recursively

--- week3/day5/Task02/ArrayChecker.java
+++ week3/day5/Task02/ArrayChecker.java
@@ -4,10 +4,19 @@ public class ArrayChecker{
         System.out.println("Symmetrical: " + ac.isSymetrical());
     }
 
-    private boolean isSymetrical(int[] arr){        
-        if(arr[position] == arr[arr.length - (position+1)]){
-            return substr;
+    private boolean isSymetrical(int[] arr){   
+
+        if(arr.length < 2)
+            return true;
+
+        if(arr.length == 2)
+            return arr[0] == arr[1];
+
+        if(arr[0] != arr[arr.length - 1]){
+            return false;
         }
+
+        return isSymetrical(copy(arr, 1, arr.length - 2));
     }
 
     private int[] reverse(int[] arr){
@@ -15,8 +24,18 @@ public class ArrayChecker{
     }
 
     public int[] copy(int[] source, int startIndex, int endIndex){
-        for(int i = 0; i < destination.length; i++){
-            writeSourceValueOr0ToDestination(i, source, destination);
+
+        int size = endIndex - startIndex + 1;
+
+        if(size < 1 || size > source.length)
+            return null;
+        
+        int[] destination = new int[size];
+
+        int j = 0;
+        for(int i = startIndex; i < destination.length; i++){
+            destination[j] = source[i];
+            j++;
         }
     }
 }
