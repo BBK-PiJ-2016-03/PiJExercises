a188026883684421fd0de7ca2c9e66287d96955b
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-09 17:27:51 +0000

    Implemented reverse

--- week3/day5/Task02/ArrayChecker.java
+++ week3/day5/Task02/ArrayChecker.java
@@ -13,6 +13,26 @@ public class ArrayChecker{
 
         int[] arr4 = new int[]{1,1,2,3,4,5,4,3,2};
         System.out.println("Symmetrical (false): " + ac.isSymetrical(arr4));
+
+        int[] arr5 = new int[]{1, 2, 3, 4, 5};
+        System.out.print("Symmetrical (5,4,3,2,1): ");
+        for(int number : ac.reverse(arr5)){        
+            System.out.print(number+",");
+        }
+        System.out.println();
+
+        int[] arr6 = new int[]{};
+        System.out.print("Symmetrical (): ");
+        for(int number : ac.reverse(arr6)){        
+            System.out.print(number+",");
+        }
+        System.out.println();
+
+        int[] arr7 = new int[]{1};
+        System.out.print("Symmetrical (1): ");
+        for(int number : ac.reverse(arr7)){        
+            System.out.print(number+",");
+        }
     }
 
     private boolean isSymetrical(int[] arr){   
@@ -35,7 +55,13 @@ public class ArrayChecker{
         return false;
     }
 
-    private int[] reverse(int[] arr){
-        return new int[0];
+    private int[] reverse(int[] forward){
+        int[] reversed = new int[forward.length];
+
+        for(int i = 0; i < reversed.length; i++){
+            reversed[i] = forward[forward.length - 1 - i];
+        }
+
+        return reversed;
     }
 }
