a0cfa9dca15cf58ddbb02a10c4046fd59752457a
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-09 16:06:38 +0000

    Started implementing test for IntSet interface when implemented

---
+++ week6/day12/task04/IntSetImplTest.java
@@ -0,0 +1,20 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class IntSetImplTest{
+
+    @Test
+    public void testAdd(){
+
+    }
+
+    @Test
+    public void testContains(){
+        
+    }
+
+    @Test
+    public void testContainsVerbose(){
+        
+    }
+}
