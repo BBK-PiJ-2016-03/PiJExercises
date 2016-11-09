b7a751e81bcb58a2eb494821c83e21a73d334886
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-09 15:02:33 +0000

    Implemented and ran junit tests

--- week6/day12/task02/Tests.java
+++ week6/day12/task02/Tests.java
@@ -5,10 +5,24 @@ public class Tests{
     
     @Test
     public void testShortHash(){        
+        int hash;        
         for(int i = 0; i < 5000; i++){      
-            int hash = HashUtilities.shortHash(i);
-            assertTrue(String.format("%d returns a hash: %d that is out of range", i, hash), 0 <= hash && hash <= 1000);
+            int randomInput = getRand(Integer.MAX_VALUE);
+            hash = HashUtilities.shortHash(randomInput);
+            assertTrue(String.format("%d returns a hash: %d that is out of range", randomInput, hash), 0 <= hash && hash <= 1000);
         }
+
+        int[] cornerCases = new int[]{0, -Integer.MAX_VALUE, Integer.MAX_VALUE};
+
+        for(int input : cornerCases){
+            hash = HashUtilities.shortHash(input);
+            assertTrue(String.format("%d returns a hash: %d that is out of range", input, hash), 0 <= hash && hash <= 1000);
+        } 
+
+    }
+
+    private int getRand(int max){
+        return (int) Math.abs(Math.floor(Math.random() * (max+1)));
     }
 }
 
