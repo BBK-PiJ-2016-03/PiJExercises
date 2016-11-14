65339f39df70f238c780648e9629e4bfa5d66e3f
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-14 13:01:52 +0000

    Added User interface"

---
+++ week7/day13 - TDD/Task01 - Library Book Tracker/User.java
@@ -0,0 +1,13 @@
+public interface User{
+    /**
+     * get the user's library ID
+     * @return int libraryID
+     */
+    int getLibraryID();
+
+    /**
+     * get the user's name
+     * @return String name
+     */
+    String getName();    
+}
