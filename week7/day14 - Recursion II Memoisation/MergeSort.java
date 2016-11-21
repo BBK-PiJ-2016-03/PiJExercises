a723b967789afe293341f6e1cf871d13c430c0f0
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-21 12:04:58 +0000

    Cleared compile errors

--- week7/day14 - Recursion II Memoisation/MergeSort.java
+++ week7/day14 - Recursion II Memoisation/MergeSort.java
@@ -10,8 +10,8 @@ public class MergeSort<T extends Comparable<T>>{
 
         int midIndex = unsorted.size() / 2;
 
-        List<T> leftHalf = getLeftHalfOfList(unsorted);
-        List<T> rightHalf = getRightHalfOfList(unsorted);
+        List<T> leftHalf = getLeftHalfOfList(unsorted, midIndex);
+        List<T> rightHalf = getRightHalfOfList(unsorted, midIndex);
 
         return merge(sort(leftHalf), sort(rightHalf));
     }
@@ -22,6 +22,28 @@ public class MergeSort<T extends Comparable<T>>{
 
     private List<T> getRightHalfOfList(List<T> list, int midIndex){
         return copyList(list, midIndex+1, list.size()-1);
+    }
+
+    private List<T> merge(List<T> left, List<T> right){
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        List<T> mergedList = new ArrayList<>();
+
+        while(leftIndex < left.size() || rightIndex < right.size()){
+            if(left.get(leftIndex).compareTo(right.get(rightIndex)) >= 0 && leftIndex < left.size()){
+                mergedList.add(left.get(leftIndex));
+                leftIndex++;
+            }
+            else if(rightIndex < right.size()){
+                mergedList.add(right.get(rightIndex));
+                rightIndex++;
+            }    
+            else{
+                break;
+            }
+        }
+        return mergedList;
     }
 
     private List<T> copyList(List<T> list, int from, int to){
