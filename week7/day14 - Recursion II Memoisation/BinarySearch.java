a0df3de741b33e214bbd4da8bc5c5ccf2b1ef65d
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-21 09:42:58 +0000

    Implemented Binary Search

--- week7/day14 - Recursion II Memoisation/BinarySearch.java
+++ week7/day14 - Recursion II Memoisation/BinarySearch.java
@@ -7,19 +7,54 @@ public class BinarySearch{
     }
 
     private void start(){
-        List sortedList = generateSortedList();
-        bin.search();
+        List<Integer> sortedList = generateSortedList(20);
+        int target = -3;
+        String output = search(sortedList, target) ? "Target located" : "Target not found";
+        System.out.println(output);    
     }
 
-    private List generateSortedList(int length){
+    private List<Integer> generateSortedList(int length){
         List<Integer> list = new ArrayList<>();
-        for(int i = 0; i < length; i++){
+        for(int i = 1; i <= length; i++){
             list.add(i);
         }
         return list;
     }
 
-    private void search(List sortedList){
+    private boolean search(List<Integer> sortedList, int target){
+        int midIndex = sortedList.size() / 2;
+        int midElement = sortedList.get(midIndex);
 
+        if(midElement == target) 
+            return true;
+
+        if(sortedList.size() == 1)
+            return false;
+
+        if(target < midElement){
+            List<Integer> halfList = getLeftHalfOfList(sortedList, midIndex);
+            return search(halfList, target);
+        }
+        
+        List<Integer> halfList = getRightHalfOfList(sortedList,midIndex);
+        return search(halfList, target);        
+    }
+
+    private List<Integer> getLeftHalfOfList(List<Integer> sortedList, int midIndex){
+        return copyList(sortedList, 0, midIndex-1);
+    }
+
+    private List<Integer> getRightHalfOfList(List<Integer> sortedList, int midIndex){
+        return copyList(sortedList, midIndex+1, sortedList.size()-1);
+    }
+
+    private List<Integer> copyList(List<Integer> sortedList, int from, int to){
+        List<Integer> reducedList = new ArrayList<>();
+
+        for(int i = from; i <= to; i++){
+            reducedList.add(sortedList.get(i));
+        }
+
+        return reducedList;
     }
 }
