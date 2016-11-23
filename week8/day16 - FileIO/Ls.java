9dcf5d0e6d40d26231f4065cf0869c864c70f721
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-23 12:46:24 +0000

    Completed Ls

--- week8/day16 - FileIO/Ls.java
+++ week8/day16 - FileIO/Ls.java
@@ -1,3 +1,15 @@
+import java.io.*;
+
 public class Ls{
-    
+    public static void main(String[] args){
+        System.out.println(System.getProperty("user.dir"));
+        File file = new File(System.getProperty("user.dir"));
+
+        String[] files = file.list();
+        if(files != null){
+            for(String currentFile : files){
+                System.out.println(currentFile);
+            }
+        }
+    }
 }
