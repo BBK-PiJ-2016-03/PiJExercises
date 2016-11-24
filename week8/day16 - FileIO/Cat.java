cca509d994d444610f025a5027fc8618d60b0757
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-24 11:20:24 +0000

    Cat completed

--- week8/day16 - FileIO/Cat.java
+++ week8/day16 - FileIO/Cat.java
@@ -1,3 +1,55 @@
-public class Cat{
-    
+import java.io.*;
+
+public class Cat {
+
+	public static void main (String [] args){	
+		Cat testCat = new Cat();
+		if(args.length < 1){
+            System.out.println("No arguments entered");
+            return;
+		}
+
+        testCat.run(args[0]);
+	}
+	
+	public void run(String filePath){	
+		File file = new File(filePath);
+
+        try{
+            if(fileIsValid(file))
+                printFileContents(file);
+        }	
+        catch(IOException e)	{
+            System.out.println(e.getMessage());
+        }
+	}
+
+    private boolean fileIsValid(File file) throws IOException{
+        if(!file.exists())
+            throw new IOException(String.format("File %s does not exist.", file.getName()));        
+
+		if(file.isDirectory())
+            throw new IOException("Directory already exists");
+		
+        return true;
+    }
+	
+	public void printFileContents (File file){
+		try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+			printBufferedOutput(reader);
+		} 
+        catch (FileNotFoundException e){
+			e.printStackTrace();
+		} 
+        catch (IOException e){
+			e.printStackTrace();	
+		}
+	}
+
+    private void printBufferedOutput(BufferedReader reader) throws IOException{
+        String output;
+        while((output = reader.readLine()) != null){
+            System.out.println(output);
+        }
+    }
 }
