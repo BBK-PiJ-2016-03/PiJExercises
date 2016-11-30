646c7a8aae91e5b981814c91e83045b588638a7b
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-11-30 17:52:14 +0000

    fixed notification issue

---
+++ week9/day18 - Executors/task03 - Responsive UI/Notification.java
@@ -0,0 +1,13 @@
+public class Notification implements Runnable{
+
+    private Ui origin;
+
+    public Notification(Ui origin){
+        this.origin = origin;
+    }
+
+    public void run(){
+
+    }
+
+}
