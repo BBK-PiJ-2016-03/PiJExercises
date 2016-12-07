b484fa6b558b4ef962d9b9e2a3306567640d9479
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-12-07 18:05:32 +0000

    Added tests for the always true lambda

--- week10/day19 - Lambdas/task03/StringUtils2Tests.java
+++ week10/day19 - Lambdas/task03/StringUtils2Tests.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StringUtils2Tests {
 
     @Test
-    public void testBetterStringSuccess(){
+    public void testBetterStringSuccessExample1(){
         String string1 = "Better";
         String string2 = "Worse";
         Boolean response = StringUtils2.betterString(string1, string2, (String s1, String s2) -> s1.length() > s2.length());
@@ -16,12 +16,32 @@ public class StringUtils2Tests {
     }
 
     @Test
-    public void testBetterStringFailure(){
+    public void testBetterStringFailureExample1(){
         String string1 = "Better";
         String string2 = "Worse";
         Boolean response = StringUtils2.betterString(string1, string2, (String s1, String s2) -> s1.length() < s2.length());
         Boolean expected = false;
         assertEquals(expected, response);
     }
+
+    @Test
+    public void testBetterStringSuccessExample2(){
+        String string1 = "Better";
+        String string2 = "Worse";
+        Boolean response = StringUtils2.betterString(string1, string2, (s1, s2) -> true);
+        Boolean expected = true;
+        assertEquals(expected, response);
+    }
+
+    @Test
+    public void testBetterStringFailureExample2(){
+        String string1 = "Better";
+        String string2 = "Worse";
+        Boolean response = StringUtils2.betterString(string1, string2, (s1, s2) -> true);
+        Boolean expected = true;
+        assertEquals(expected, response);
+    }
+
+
 
 }
