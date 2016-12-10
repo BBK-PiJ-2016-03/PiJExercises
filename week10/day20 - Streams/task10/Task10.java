e437e122af7f948416469ae8a7028742dffdbc27
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-12-10 11:47:57 +0000

    Implemented runs and initial source data ( doubles will probably have to be increased in size to demonstrate issues with the parallel processing

--- week10/day20 - Streams/task10/Task10.java
+++ week10/day20 - Streams/task10/Task10.java
@@ -6,28 +6,35 @@ import java.util.List;
  */
 public class Task10 {
 
-    private List<Integer> numbers = Arrays.asList(5, 20, 32, 43);
+    private static final List<Integer> numbers = Arrays.asList(5, 20, 32, 43);
+    private static final List<Double> doubles = Arrays.asList(5.0, 20.0, 32.0, 43.0);
 
     public static void main(String[] args) {
+        System.out.println(sum1(numbers));
+        System.out.println(sum2(numbers));
+        System.out.println(sum3(numbers));
+        System.out.println(parallelSum(numbers));
+        System.out.println(doublesProductSerial(doubles));
+        System.out.println(doublesProductParallel(doubles));
     }
 
 //    (a) (*) Provide three ways to use streams to compute the sum of a list of numbers
-    public int sum1(List<Integer> input){
+    public static int sum1(List<Integer> input){
         return 0;
     }
 
-    public int sum2(List<Integer> input){
+    public static int sum2(List<Integer> input){
         return 0;
     }
 
-    public int sum3(List<Integer> input){
+    public static int sum3(List<Integer> input){
         return 0;
     }
 
 //    (b) (*) Rewrite one of the solutions for (10a) so that it can be executed in parallel; verify
 //    that you get the same answer as for the sequential code.
 
-    public int parallelSum(List<Integer> input){
+    public static int parallelSum(List<Integer> input){
         return 0;
     }
 
@@ -36,11 +43,11 @@ public class Task10 {
 //            (Note: this is a bit tricky, because it seems at first that multiplication is associative, as
 //    required by the parallel reduce. It will be impossible to illustrate the result if you have
 //    a single-core computer.)
-    public int doublesProductSerial(List<Double> input){
+    public static int doublesProductSerial(List<Double> input){
         return 0;
     }
 
-    public int doublesProductParallel(List<Double> input){
+    public static int doublesProductParallel(List<Double> input){
         return 0;
     }
 }
