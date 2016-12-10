71f7034ad6ed7847227fb1ffff7b3bf6cf128ed4
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-12-10 11:09:15 +0000

    Implemented task 7b

--- week10/day20 - Streams/task07/Task07.java
+++ week10/day20 - Streams/task07/Task07.java
@@ -14,27 +14,89 @@ public class Task07 {
     }
 
     private void run(){
-        process();
+        runProcess();
+        runProcessShowLazy();
     }
 
-    private void process() {
-        String eresult = words.stream()
+    private void runProcess() {
+        runProcessEResult();
+        runProcessQResult();
+    }
+
+    private void runProcessEResult() {
+        System.out.println("--- Processing E Result ---");
+        processEResult();
+        System.out.println();
+    }
+
+    private void runProcessQResult() {
+        System.out.println("--- Processing Q Result ---");
+        processQResult();
+        System.out.println();
+    }
+
+    private void processEResult() {
+        String eResult = words.stream()
                 .map(String::toUpperCase)
                 .filter(w -> w.length() < 4)
                 .filter(w -> w.contains("E"))
                 .limit(1)
                 .collect(Collectors.joining());
 
-        System.out.println("eresult: "+eresult);
+        System.out.println("eResult: "+eResult);
+    }
 
-        String qresult = words.stream()
+    private void processQResult() {
+        String qResult = words.stream()
                 .map(String::toUpperCase)
                 .filter(w -> w.length() < 4)
                 .filter(w -> w.contains("Q"))
                 .limit(1)
                 .collect(Collectors.joining());
 
-        System.out.println("qresult: "+qresult);
+        System.out.println("qResult: "+qResult);
+    }
+
+    private void runProcessShowLazy() {
+        runProcessLazyEResult();
+        runProcessLazyQResult();
+    }
+
+    private void runProcessLazyEResult() {
+        System.out.println("--- Processing Lazy E Result ---");
+        processLazyEResult();
+        System.out.println();
+    }
+
+    private void runProcessLazyQResult() {
+        System.out.println("--- Processing Lazy Q Result ---");
+        processLazyQResult();
+        System.out.println();
+    }
+
+    private void processLazyEResult() {
+        String eResult = words.stream()
+                .map(String::toUpperCase)
+                .peek(System.out::println)
+                .filter(w -> w.length() < 4)
+                .filter(w -> w.contains("E"))
+                .limit(1)
+                .peek(System.out::println)
+                .collect(Collectors.joining());
+
+        System.out.println("eResult: "+eResult);
+    }
+
+    private void processLazyQResult() {
+        String qResult = words.stream()
+                .map(String::toUpperCase)
+                .peek(System.out::println)
+                .filter(w -> w.length() < 4)
+                .filter(w -> w.contains("Q"))
+                .limit(1)
+                .collect(Collectors.joining());
+
+        System.out.println("qResult: "+qResult);
     }
 
 }
