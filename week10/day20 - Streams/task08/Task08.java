77766c5b3d2e7e99e8c71a6847d6baad86f3623d
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-12-10 11:16:44 +0000

    Added question methods

--- week10/day20 - Streams/task08/Task08.java
+++ week10/day20 - Streams/task08/Task08.java
@@ -16,4 +16,22 @@ public class Task08 {
 
     }
 
+    private void a(){
+//        (a) Produce a single String that is the result of concatenating the uppercase versions of all
+//        of the Strings. E.g., the result should be "HIHELLO...". Use a single reduce operation,
+//        without using map.
+
+    }
+
+    private void b(){
+//        (b) Produce the same String as above, but this time via a map operation that turns the
+//        words into upper case, followed by a reduce operation that concatenates them.
+
+    }
+
+    private void c(){
+//        (c) (*) Produce a String that is all the words concatenated together, but with commas in
+//        between. E.g., the result should be "hi,hello,...". Note that there is no comma at
+//        the beginning, before “hi”, and also no comma at the end, after the last word.
+    }
 }
