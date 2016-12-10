4ff1af0dfaa5ca9e4b982ff1dc812c85103290a9
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-12-10 11:15:05 +0000

    Task 8 initial commit

---
+++ week10/day20 - Streams/task08/Task08.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Alexander Worton on 09/12/2016.
+ */
+public class Task08 {
+    private static final List<String> words = Arrays.asList("hiq", "hel", "hoe", "har", "he", "hub");
+
+    public static void main(String[] args) {
+        Task08 task = new Task08();
+        task.run();
+    }
+
+    private void run(){
+
+    }
+
+}
