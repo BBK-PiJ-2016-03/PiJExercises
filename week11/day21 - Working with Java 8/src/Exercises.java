d26e83c2766d33c114f178884599adce8300582d
Alexander Worton <aworto01@mail.bbk.ac.uk>
2016-12-12 16:48:24 +0000

    Completed Task 6

--- week11/day21 - Working with Java 8/src/Exercises.java
+++ week11/day21 - Working with Java 8/src/Exercises.java
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -127,9 +128,12 @@ public class Exercises {
     // Exercise 6: Find the length of the longest line in the file.
 
     @Test
-    @Ignore
     public void lengthOfLongestLine() throws IOException {
         int longest = 0; /* TODO */
+        longest = reader.lines()
+                .map(String::length)
+                .max(Comparator.naturalOrder())
+                .get();
 
         assertEquals(longest, 53);
     }
